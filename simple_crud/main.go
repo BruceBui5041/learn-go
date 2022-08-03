664b68fd65fe6c325db11e7bf1f9a87d922521ee
BruceBui5041 <brucebui5041@gmail.com>
2022-08-03 20:59:07 +0700

    Update main.go

--- simple_crud/main.go
+++ simple_crud/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/spf13/viper"
@@ -8,15 +9,19 @@ import (
 	"gorm.io/gorm"
 )
 
-/**
-	omitempty: if value is nil, 0, "" then the key will be removed
-**/
+// omitempty: if value is nil, 0, "" then the key will be removed
 type Note struct {
 	Id      int    `json:"id,omitempty" gorm:"column:id"`
 	Title   string `json:"title" gorm:"column:title"`
 	Content string `json:"content" gorm:"column:content"`
 }
 
+// *string: so now we can update title = ""
+type NoteUpdate struct {
+	Title *string `json:"title" gorm:"column:title"`
+}
+
+// TableName overrides the table name used by Note to `notes`
 func (Note) TableName() string {
 	return "notes"
 }
@@ -32,6 +37,28 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	newNote := Note{Title: "Test Note", Content: "Test content"}
-	db.Create(newNote)
+	db.AutoMigrate(&Note{})
+	/**
+		Insert
+	**/
+	// newNote := Note{Title: "Test Note 3", Content: "Test content 3"}
+
+	// if err := db.Create(&newNote); err != nil {
+	// 	fmt.Println(err)
+	// }
+
+	var notes []Note
+	db.Where("content IS NOT NULL").Find(&notes)
+
+	var note Note
+	if err := db.Where("id = ?", 3).First(&note); err == nil {
+		log.Println(err)
+	}
+
+	newTitle := ""
+	db.Updates(&NoteUpdate{Title: &newTitle})
+
+	db.Table(Note{}.TableName()).Where("id = ?", 5).Delete(nil)
+
+	fmt.Println(notes)
 }
