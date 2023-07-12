76aa3d4ad4961c3a76fc8010fe9d0fcfa5133d8c
Bui Hong Lan <lan@hydrax.io>
2023-07-13 00:52:30 +0700

    Update appsocketio engine.go

--- food_delivery_be/appsocketio/engine.go
+++ food_delivery_be/appsocketio/engine.go
@@ -2,17 +2,25 @@ package appsocketio
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"learn-go/food_delivery_be/component"
+	"learn-go/food_delivery_be/component/tokenprovider/jwt"
+	"learn-go/food_delivery_be/modules/user/userstorage"
 	"sync"
 
 	"github.com/gin-gonic/gin"
 	socketio "github.com/googollee/go-socket.io"
+	"github.com/googollee/go-socket.io/engineio"
+	"github.com/googollee/go-socket.io/engineio/transport"
+	"github.com/googollee/go-socket.io/engineio/transport/websocket"
 )
 
 type RealtimeEngine interface {
-	UserSockets(userId int) []AppSocket
+	UserSockets(userId int) []AppSocket // Note: this is because 1 user can use more than 1 device
 	EmmitToRoom(room string, key string, data interface{}) error
 	EmmitToUser(userId int, key string, data interface{}) error
-	Run(ctx context.Context, engine *gin.Engine)
+	Run(ctx component.AppContext, ginEngine *gin.Engine) error
 }
 
 type rtEngine struct {
@@ -82,6 +90,71 @@ func (engine *rtEngine) EmmitToUser(userId int, key string, data interface{}) er
 	for _, s := range sockets {
 		s.Emit(key, data)
 	}
+
+	return nil
+}
+
+func (engine *rtEngine) Run(appCtx component.AppContext, ginEngine *gin.Engine) error {
+	server, _ := socketio.NewServer(&engineio.Options{
+		// ép kiểu về websocket luôn vi có thể nó sẽ tạo ra transport theo Long-polling nếu client k support
+		Transports: []transport.Transport{websocket.Default},
+	})
+
+	server.OnConnect("/", func(c socketio.Conn) error {
+		fmt.Println("Connected: ", c.ID(), " Ip:", c.RemoteAddr())
+
+		return nil
+	})
+
+	server.OnError("/", func(c socketio.Conn, err error) {
+		fmt.Println("Connect error: ", err)
+	})
+
+	server.OnDisconnect("/", func(c socketio.Conn, reason string) {
+		fmt.Println("Disconnected: ", reason)
+	})
+
+	server.OnEvent("/", "authenticate", func(c socketio.Conn, token string) {
+		db := appCtx.GetMainDBConnection()
+		store := userstorage.NewSQLStore(db)
+		tokenProvider := jwt.NewTokenJWTProvider(appCtx.SecretKey())
+
+		user_payload, err := tokenProvider.Validate(token)
+
+		if err != nil {
+			c.Emit("authentication failed", err.Error())
+			c.Close()
+			return
+		}
+
+		user, err := store.FindUser(context.Background(), map[string]interface{}{"id": user_payload.UserId})
+
+		if err != nil {
+			c.Emit("authentication failed", err.Error())
+			c.Close()
+			return
+		}
+
+		if user.Status == 0 {
+			c.Emit("authentication failed", errors.New("you has been banned or deleted"))
+			c.Close()
+			return
+		}
+
+		appSkt := NewAppSocket(c, user)
+		engine.saveAppSocket(user.Id, appSkt)
+
+		user.Mask(false)
+
+		c.Emit("authenticated", user)
+	})
+
+	go server.Serve()
+
+	// bản chất của socket.io nó phải từ http đi lên
+	// nên phải có 1 req đi lên để xin quyền upgrade
+	ginEngine.GET("/socket.io/*any", gin.WrapH(server))
+	ginEngine.POST("/socket.io/*any", gin.WrapH(server))
 
 	return nil
 }
